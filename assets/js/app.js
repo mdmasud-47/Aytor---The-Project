262d71cfa373a4828f525bf18b301ba8308f1cc0
Masud <mdmoksod77@gmail.com>
2023-10-21 00:43:38 +0600

    Back to top button

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,6 +50,22 @@ $(function() {
         dots: true,
         slidesToShow:4,
     })
+
+    // BackToTop Button
+    $(window).on('scroll',function() {
+        let scrollTop = $(window).scrollTop();
+
+        if(scrollTop > 600){
+             $('#backToTop').fadeIn(500);
+        }else{
+            $('#backToTop').fadeOut(500);
+        }
+    })
+    $("#backToTop").on('click',function(){
+        $('html, body').animate({
+            scrollTop:0
+        }, 200)
+    })
     
 });
 // Activate Tooltips
